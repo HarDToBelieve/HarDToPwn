293ae1a4457ee806fbe48cf1348549d42c185d24
Đỗ Minh Tuấn <tuanit96@gmail.com>
2016-06-04 00:21:44 +0700

    Add files via upload
    Fixed some stupid errors

--- hardtobelieve.py
+++ hardtobelieve.py
@@ -1,8 +1,19 @@
 import sys, telnetlib, time, socket, struct
 from subprocess import Popen, PIPE
-import Tkinter, select
+import tkinter, select
+import string
+
+def processStr(str):
+	res = ""
+	for ch in str:
+		if chr(ch) in string.printable:
+			res += chr(ch)
+		else:
+			res += "\\" + hex(ch)[1:]
+	return res
 
 tmp = None
+
 class HarDToSock:
 	def __init__(self, *args, **kwargs):
 		self.magicValue = 1928
@@ -14,7 +25,7 @@ class HarDToSock:
 			self.host = kwargs["host"]
 			self.port = kwargs["port"]
 		else:
-			print "[-] Failed in connecting"
+			print ("[-] Failed in connecting")
 			sys.exit()
 		self.sock = socket.create_connection((self.host, self.port))
 		if "timeout" in kwargs:
@@ -25,43 +36,48 @@ class HarDToSock:
 		ready = select.select([self.sock], [], [], self.timeout)
 		if ready[0]:
 			if len(kwargs) == 0:
-				return self.sock.recv(4096)
+				return processStr(self.sock.recv(4096))
 			elif "end" in kwargs:
 				s = kwargs["end"]
 				buf = ''
 				while not buf.endswith(s):
-					buf += self.sock.recv(1)
-				return buf
+					tmp = self.sock.recv(1)
+					if tmp:
+						buf += tmp
+					else:
+						print ("[-] Cannot find that ending")
+						sys.exit()
+				return processStr(buf)
 			else:
-				print "[-] Need a purpose to wait!"
+				print ("[-] Need a purpose to wait!")
 				sys.exit()
 		else:
-			print "[+] No return message"
+			print ("[+] No return message")
 			return ""
 	def send(self, payload):
-		self.sock.send(payload)
+		self.sock.send(payload.encode())
 
-	def sendln(self, payload):
-		self.sock.send(payload + "\n")
+	def sendline(self, payload):
+		self.sock.send((payload + "\n").encode())
 
-	class Terminal(Tkinter.Frame):
+	class Terminal(tkinter.Frame):
 		def Enter_pressed(self):
-			input_get = self.input.get("1.0", Tkinter.END)
+			input_get = self.input.get("1.0", tkinter.END)
 			if input_get.split('\n')[-2] == 'exit':
 				sys.exit()
 			if ( tmp != None ) and ( input_get.split('\n')[-2] ):
-				tmp.sendln(input_get.split('\n')[-2])
+				tmp.sendline(input_get.split('\n')[-2])
 				string = ""
 				tmp.sock.setblocking(0)
 				ready = select.select([tmp.sock], [], [], tmp.timeout)
 				if ready[0]:
 					string = tmp.recv()
 				else:
-					print "[+] No return message"
-				if string[:-1] != '\n' and len(self.output.get("1.0", Tkinter.END)) > 1:
-					self.output.insert(Tkinter.INSERT, "\n")
-				self.output.insert(Tkinter.INSERT, string)
-				self.output.see(Tkinter.END)
+					print ("[+] No return message")
+				if string[:-1] != '\n' and len(self.output.get("1.0", tkinter.END)) > 1:
+					self.output.insert(tkinter.INSERT, "\n")
+				self.output.insert(tkinter.INSERT, string)
+				self.output.see(tkinter.END)
 
 		def onResize(self, event):
 			self.width = event.width
@@ -70,18 +86,18 @@ class HarDToSock:
 			self.config(width=self.width, height=self.height)
 
 		def __init__(self, parent, *args, **kwargs):
-			Tkinter.Frame.__init__(self, parent, *args, **kwargs)
+			tkinter.Frame.__init__(self, parent, *args, **kwargs)
 			self.parent = parent
 			self.bind("<Configure>", self.onResize)
 
-			self.label = Tkinter.Label(self, text="Nothing is impossible\nImpossible says its self \"I\'m possible\"")
-			self.interact = Tkinter.Frame(self)
+			self.label = tkinter.Label(self, text="Nothing is impossible\nImpossible says its self \"I\'m possible\"")
+			self.interact = tkinter.Frame(self)
 
-			self.input = Tkinter.Text(self.interact, width=40, height=24, fg="green", bg="black", font = "Helvetica 13 bold")
+			self.input = tkinter.Text(self.interact, width=40, height=24, fg="light green", bg="black", font = "Helvetica 13 bold")
 			self.input.bind("<Return>", lambda x: self.Enter_pressed())
 			self.input.focus_set()
 			
-			self.output = Tkinter.Text(self.interact, width=60, height=24, fg="green", bg="black", font = "Helvetica 13 bold")
+			self.output = tkinter.Text(self.interact, width=60, height=24, fg="light green", bg="black", font = "Helvetica 13 bold")
 
 			self.label.pack()
 			self.interact.pack()
@@ -90,7 +106,7 @@ class HarDToSock:
 			
 	def hardtopwn(self):
 		global tmp
-		print "[+] Recveiving the remain bytes..."
+		print ("[+] Recveiving the remain bytes...")
 		while True:
 			self.sock.setblocking(0)
 			ready = select.select([self.sock], [], [], self.timeout)
@@ -99,8 +115,8 @@ class HarDToSock:
 				continue
 			else:
 				break
-		print "[+] Keep calm and try harder"
-		root = Tkinter.Tk()
+		print ("[+] Keep calm and try harder")
+		root = tkinter.Tk()
 		root.geometry("923x504")
 		root.resizable(width=False, height=False)
 		root.title("Terminal")
@@ -110,24 +126,28 @@ class HarDToSock:
 
 
 def p32(number):
-	return struct.pack('<I', number)
+	return struct.pack('<I', number).decode()
 
 def u32(s):
-	return struct.unpack('<I', s)[0]
+	tmp = s[:]
+	while len(tmp) < 4:
+		tmp += b'\x00'
+	return struct.unpack('<I', tmp)[0]
 
 def p64(number):
-	return struct.pack('<Q', number)
+	return struct.pack('<Q', number).decode()
 
 def u64(s):
 	tmp = s[:]
 	while len(tmp) < 8:
-		tmp += "\x00"
+		tmp += b'\x00'
 	return struct.unpack('<Q', tmp)[0]
 def log(s):
-	print "[+]", s
+	print ("[+] " + s)
 
 def shcode32():
 	return "\x6a\x0b\x58\x99\x52\x66\x68\x2d\x70\x89\xe1\x52\x6a\x68\x68\x2f\x62\x61\x73\x68\x2f\x62\x69\x6e\x89\xe3\x52\x51\x53\x31\xc9\xcd\x80"
 
 def shcode64():
 	return "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+
